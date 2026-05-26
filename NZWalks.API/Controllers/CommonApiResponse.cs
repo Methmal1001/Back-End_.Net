e0f4e973e986fb77b02338f8f4ff7ba55081d471
Methmal1001 <hau.methmal1@gmail.com>
2026-05-26 15:01:42 +0530

    new changes added

---
+++ NZWalks.API/Controllers/CommonApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace NZWalks.API.Controllers
+{
+    internal class CommonApiResponse<T>
+    {
+        public int StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
---
+++ NZWalks.API/Controllers/CommonDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿using NZWalks.API.Models.DTO.Product;
+
+namespace NZWalks.API.Controllers
+{
+    internal class CommonDetailsDto
+    {
+        public int StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public ProductListResponseDto Data { get; set; }
+    }
+}
