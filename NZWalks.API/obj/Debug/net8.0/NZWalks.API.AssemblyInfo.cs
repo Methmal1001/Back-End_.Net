5aa0d7a8d9ef125847edd7a503e9c9c913ba3de6
Methmal1001 <hau.methmal1@gmail.com>
2026-07-06 16:33:01 +0530

    Update local build output

--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a19c27f157d3e02f7c30563cb5b431fd05d649f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3efa90e8905accfbe82c2bb2f976c4f5bb4b04e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
